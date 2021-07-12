34df3102f1b55d73835e8d36075c6d0f9e13b199
Uffe Koch <uffe@detgodefirma.dk>
2021-07-12 08:42:20 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.showNumber(10)
+basic.showNumber(9)
+basic.showNumber(8)
+basic.showNumber(7)
+basic.showNumber(6)
+basic.showNumber(5)
+basic.showNumber(4)
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+basic.forever(function () {
+	
+})
